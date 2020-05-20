cc2507aa99bc122221bdf30c93f929fedb217974
wenqi73 <1264578441@qq.com>
2020-05-21 00:47:41 +0800

    release: 0.1.2

--- publish.sh
+++ publish.sh
@@ -1,20 +1,20 @@
 echo '[Action] git push to another repo'
-ls ${RELEASE_PRIMARYARTIFACTSOURCEALIAS}
-ls ${RELEASE_PRIMARYARTIFACTSOURCEALIAS}/drop
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 site_name=nz-locale-sync-release
-repo=https://github.com/wenqi73/${site_name}.git
 git config --global core.longpaths true
 git config --global user.name "wenqi73"
-git clone "${repo}" --depth=1
-rsync -av --delete --exclude={'.DS_Store','.git*'} ./dist/ ./${site_name}/
-cd nz-locale-sync-release
+git config --global user.email "1264578441@qq.com"
+git clone https://github.com/wenqi73/${site_name}.git --depth=1
+rsync -av --delete --exclude={'.DS_Store','.git*'} ${ROOT}/dist/ ./${site_name}/
+cd ${site_name}
 git add .
-git commit -m "sync from azure devops"
-git push ${repo}
+git commit -m "sync from azure ${BUILD_PULLREQUEST_SOURCEBRANCH}"
+git push https://$(git-token)@github.com/wenqi73/${site_name}.git
 
 echo '[Action] logging package files'
-cd ../dist
+cd ${ROOT}/dist
 ls
+cat package.json
 echo '[Action] adding token to npmrc'
 echo '//registry.npmjs.org/:_authToken=$(npm-token)' > .npmrc
 

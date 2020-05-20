2d5e8b8f37f7a836b721773f694a94c549b3a12d
wenqi73 <1264578441@qq.com>
2020-05-20 23:24:10 +0800

    release: 0.1.2

--- publish.sh
+++ publish.sh
@@ -1,17 +1,19 @@
 echo '[Action] git push to another repo'
+ls ${RELEASE_PRIMARYARTIFACTSOURCEALIAS}
+ls ${RELEASE_PRIMARYARTIFACTSOURCEALIAS}/drop
 site_name=nz-locale-sync-release
 repo=https://github.com/wenqi73/${site_name}.git
 git config --global core.longpaths true
 git config --global user.name "wenqi73"
 git clone "${repo}" --depth=1
-rsync -av --delete --exclude={'.DS_Store','.git*'} ./extract/package/ ./${site_name}/
+rsync -av --delete --exclude={'.DS_Store','.git*'} ./dist/ ./${site_name}/
 cd nz-locale-sync-release
 git add .
 git commit -m "sync from azure devops"
 git push ${repo}
 
 echo '[Action] logging package files'
-cd ../extract/package
+cd ../dist
 ls
 echo '[Action] adding token to npmrc'
 echo '//registry.npmjs.org/:_authToken=$(npm-token)' > .npmrc
